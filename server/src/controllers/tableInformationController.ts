c4c76f60542b32b0c023b36e31398437f8a47ebe
Anathasya1 <anathasya0108@gmail.com>
2026-04-02 21:18:29 +0700

    tbc

--- server/src/controllers/tableInformationController.ts
+++ server/src/controllers/tableInformationController.ts
@@ -10,3 +10,26 @@ export const getAllTable = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Get All Table error" });
     }
 }
+
+export const getTableById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const table = await TableInformation.findByPk(id as string);
+
+        if (!table) {
+            return res.status(404).json({
+                status: "Fail",
+                message: "Table dengan ID tersebut tidak ditemukan"
+            })
+        }
+
+        return res.status(200).json({
+            status: "Success",
+            data: table
+        })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Get Table by ID error" })
+    }
+}
