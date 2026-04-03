3ea7628edc8705b125625c2f5a4d729c4ac861c9
NFYouVel <marvelnathanael072006@gmail.com>
2026-04-03 23:06:11 +0700

    create new branch

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 import { useNavigate } from "react-router";
 import { loginRequest } from "../services/api";
-
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import BackgroundLogin from "../components/BackgroundLogin";
 
 import "../styles/login.css"
@@ -10,6 +13,7 @@ import TextField from "@mui/material/TextField";
 function Login() {
 
     const [email, setEmail] = useState("")
+    const [showPassword, setShowPassword] = useState(false);
     const [password, setPassword] = useState("")
 
     // Navigate
@@ -37,31 +41,98 @@ function Login() {
         <div className="login-page">
             <BackgroundLogin />
             <div className="wrapper-login">
+                {/* Left Container */}
                 <div className="container-coffee"></div>
+
+                {/* Right Container */}
                 <div className="wrapper-form">
+                    {/* Logo */}
                     <div className="wrapper-login-logo">
                         <div className="login-logo"></div>
                     </div>
+                    {/* Title */}
+                    <div className="title-login">
+                        <p>Welcome back Twntiers. Please Login To See Our Cafe's Feature</p>
+                    </div>
+
+                    {/* Form */}
                     <div className="wrapper-login-form">
                         <div className="form-login">
                             <TextField
-                                id="outlined-basic"
                                 label="E-mail"
                                 variant="outlined"
+                                fullWidth
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                sx={{
+                                    marginBottom: "16px",
+                                    input: { color: "#fff" },
+                                    label: { color: "#f5e6d3" },
+                                    "& label.Mui-focused": {
+                                        color: "#f5e6d3",
+                                    },
+                                    "& .MuiOutlinedInput-root": {
+                                        borderRadius: "12px",
+                                        backgroundColor: "rgba(255,255,255,0.05)",
+                                        "& fieldset": {
+                                            borderColor: "#d2b48c",
+                                        },
+                                        "&:hover fieldset": {
+                                            borderColor: "#f5e6d3",
+                                        },
+                                        "&.Mui-focused fieldset": {
+                                            borderColor: "#f5e6d3",
+                                            borderWidth: "2px",
+                                        },
+                                    },
+                                }}
                             />
+
                             <TextField
-                                id="outlined-password-input"
                                 label="Password"
-                                type="password"
-                                autoComplete="current-password"
+                                type={showPassword ? "text" : "password"}
+                                fullWidth
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                sx={{
+                                    input: { color: "#fff" },
+                                    label: { color: "#f5e6d3" },
+                                    "& label.Mui-focused": {
+                                        color: "#f5e6d3",
+                                    },
+                                    "& .MuiOutlinedInput-root": {
+                                        borderRadius: "12px",
+                                        backgroundColor: "rgba(255,255,255,0.05)",
+                                        "& fieldset": {
+                                            borderColor: "#d2b48c",
+                                        },
+                                        "&:hover fieldset": {
+                                            borderColor: "#f5e6d3",
+                                        },
+                                        "&.Mui-focused fieldset": {
+                                            borderColor: "#f5e6d3",
+                                            borderWidth: "2px",
+                                        },
+                                    },
+                                }}
+
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                onClick={() => setShowPassword(!showPassword)}
+                                                edge="end"
+                                                sx={{ color: "#f5e6d3" }}
+                                            >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }}
                             />
+
                         </div>
                     </div>
-
 
                     <div className="login-actions">
                         <a className="logincard-navigation" onClick={handleRegisterNavigation}>REGISTER NOW</a>
