c4e8ee2ad6e32324f42b75f7ff2038e1c1df453c
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2026-04-08 12:35:20 +0700

    MERGE APPROUTES

--- client/src/routers/AppRouters.tsx
+++ client/src/routers/AppRouters.tsx
@@ -2,11 +2,17 @@ import { Routes, Route } from "react-router"
 import Login from "../pages/LoginPage"
 import Stock from "../pages/StockPage"
 import StockForm from "../pages/StockForm"
+import ForgotPasswordPage from "../pages/ForgotPasswordPage"
+import RegisterPage from "../pages/RegisterPage"
+import HomePage from "../pages/HomePage"
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path= "/stock" element={<Stock />} />
       <Route path="/stock/create" element={<StockForm />} />
       <Route path="/stock/update/:id" element={<StockForm />} />
